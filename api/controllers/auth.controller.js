4bb546d46516bf91ce9ecc2a44bb26d415376326
deepthi308 <deepthih6@gmail.com>
2025-02-24 16:12:14 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,21 +1,46 @@
 import User from "../models/user.model.js";
-import bcrypt from "bcryptjs";
+import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
-  const hashedPassword = bcrypt.hashSync(password, 10);
+  const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
-    res
-      .status(201)
-      .json({
-        success: true,
-        statusCode: 201,
-        message: "User created successfully.",
-      });
+    res.status(201).json({
+      success: true,
+      statusCode: 201,
+      message: "User created successfully.",
+    });
   } catch (error) {
     next(errorHandler(error));
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email: email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found!"));
+    }
+    console.log(validUser);
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    console.log(validPassword);
+    if (!validPassword) {
+      return next(errorHandler(401, "Wrong credentials!"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
   }
 };
